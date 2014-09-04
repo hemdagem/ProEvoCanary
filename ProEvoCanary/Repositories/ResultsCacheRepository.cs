ea8a5def1574ce1950ca567bd68ceccdb8b234b9
Hemang Rajyaguru <hemdagem@users.noreply.github.com>
2014-09-04 22:09:16 +0100

    results decorator updated. More refactoring

--- ProEvoCanary/Repositories/ResultsCacheRepository.cs
+++ ProEvoCanary/Repositories/ResultsCacheRepository.cs
@@ -10,8 +10,7 @@ namespace ProEvoCanary.Repositories
     {
         private readonly ICacheManager _cacheManager;
         private const string RecentResultsKey = "recent_results";
-        private const string HeadToHeadResultsKey = "head_to_head_results_playerOne{0}_playerTwo{1}";
-        private const string HeadToHeadRecordsKey = "head_to_head_records_playerOne{0}_playerTwo{1}";
+        private const string HeadToHeadRecordsKey = "{0}_{1}";
 
         public ResultsCacheRepository(ICacheManager cacheManager)
         {
@@ -23,12 +22,6 @@ namespace ProEvoCanary.Repositories
         public List<ResultsModel> GetResults()
         {
             return _cacheManager.Get(RecentResultsKey) as List<ResultsModel>;
-        }
-
-        public List<ResultsModel> GetHeadToHeadResults(int playerOne, int playerTwo)
-        {
-            var cachedKey = string.Format(HeadToHeadResultsKey, playerOne, playerTwo);
-            return _cacheManager.Get(cachedKey) as List<ResultsModel>;
         }
 
         public RecordsModel GetHeadToHeadRecord(int playerOne, int playerTwo)
