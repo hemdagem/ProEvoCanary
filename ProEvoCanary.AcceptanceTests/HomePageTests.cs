680cf05ea7a68b181866ca09ae59a569cf22211f
Hemang Rajyaguru <hemdagem@users.noreply.github.com>
2014-12-21 01:26:29 +0000

    Added Acceptance Tests :)

---
+++ ProEvoCanary.AcceptanceTests/HomePageTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ProEvoCanary.AcceptanceTests
+{
+    [TestFixture]
+    public class HomePageTests
+    {
+        private IWebDriver _driver;
+        const string URL = "http://www.proevoleague.hem/";
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver(@"C:\");
+        }
+
+        [Test]
+        public void ShouldHaveEventsOnTheHomePage()
+        {
+            _driver.Navigate().GoToUrl(URL);
+            IWebElement query = _driver.FindElement(By.TagName("h2"));
+
+            Assert.That(query.Text, Is.EqualTo("Events"));
+        }  
+        
+        [Test]
+        public void ShouldGoToRecordsPageWhenLinkIsClicked()
+        {
+            _driver.Navigate().GoToUrl(URL);
+            IWebElement query = _driver.FindElement(By.CssSelector("body > nav > section > ul > li:last-child > a"));
+
+            Assert.That(query.GetAttribute("href"), Contains.Substring("/Records/HeadToHead"));
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _driver.Quit();
+        }
+    }
+}
