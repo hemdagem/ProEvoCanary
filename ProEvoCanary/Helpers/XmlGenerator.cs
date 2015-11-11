1e84ead72bc8776ef19ee9b40e53f990372b6c9b
Hemang Rajyaguru <hemdagem@users.noreply.github.com>
2015-11-11 23:16:40 +0000

    big refactor started

---
+++ ProEvoCanary/Helpers/XmlGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using ProEvoCanary.Models;
+
+namespace ProEvoCanary.Helpers
+{
+	public class XmlGenerator : IXmlGenerator
+	{
+	    public string GenerateXmlDocument(List<TeamIds> teamIds, int eventId)
+	    {
+	        throw new NotImplementedException();
+	    }
+	}
+}
