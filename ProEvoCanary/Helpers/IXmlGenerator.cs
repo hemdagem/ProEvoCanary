b5d01bf79aec5bcbff61834ab7bccd1a65269a83
hemdagem <hemdagem@localhost>
2015-11-11 23:16:40 +0000

    big refactor started

---
+++ ProEvoCanary/Helpers/IXmlGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProEvoCanary.Models;
+
+namespace ProEvoCanary.Helpers
+{
+    public interface IXmlGenerator
+    {
+        string GenerateXmlDocument(List<TeamIds> teamIds, int eventId);
+    }
+}
