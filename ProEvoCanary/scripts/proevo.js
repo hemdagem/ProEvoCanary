5206b8afbfa9b9725e35d017692563e81b917363
hemdagem <hemdagem@localhost>
2016-02-21 18:21:51 +0000

    updates to admin project (still needs to be fixed)

--- ProEvoCanary/scripts/proevo.js
+++ ProEvoCanary/scripts/proevo.js
@@ -5,35 +5,5 @@ ProEvo.viewResult = function () {
     var playerOne = document.getElementById("playerOne");
     var playerTwo = document.getElementById("playerTwo");
 
-    if (window.fetch !== undefined) {
-        window.fetch('/Records/HeadToHeadResult/?' + 'playerOneId=' + playerOne.value + '&playerTwoId=' + playerTwo.value)
-                  .then(
-                    function (response) {
-                        if (response.status !== 200) {
-                            console.log('Looks like there was a problem. Status Code: ' +
-                              response.status);
-                            return;
-                        }
-
-                        // Examine the text in the response  
-                        response.json().then(function (data) {
-                            console.log(data);
-                        });
-                    }
-                  )
-                  .catch(function (err) {
-                      console.log('Fetch Error :-S', err);
-                  });
-    }
-    else {
-        window.location = "/Records/HeadToHeadResults?playerOneId=" + playerOne.value + "&playertwoId=" + playerTwo.value;
-    }
-
-
+    window.location = "/Records/HeadToHeadResults?playerOneId=" + playerOne.value + "&playertwoId=" + playerTwo.value;
 };
-
-ProEvo.updateResult = function(eventId, resultId, homeScore, awayScore) {
-    $.post("/Event/UpdateResult", { eventId: eventId, resultId: resultId, homeScore: homeScore, awayScore: awayScore }, function(data) {
-        console.log(data);
-    });
-}
